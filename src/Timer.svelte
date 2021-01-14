5f3335f3a6b6b62cb47d452769a6a71001eac94f
Felix Bores <fbores@ascentis.com>
2021-01-14 00:16:25 -0300

    html changes

---
+++ src/Timer.svelte
@@ -0,0 +1,20 @@
+<script>
+  import ProgressBar from './ProgressBar.svelte';
+</script>
+
+<style>
+  h2 {
+    margin: 0;
+  }
+  .start {
+    width: 100%;
+    background-color: rgb(174, 73, 73);
+    margin: 10px 0;
+  }
+</style>
+
+<div bp="grid">
+  <h2 bp="offset-5@md 4@md 12@sm">Seconds Left:</h2>
+</div>
+<ProgressBar />
+<button class="start">Start</button>
